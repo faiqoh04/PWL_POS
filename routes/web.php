3e83ebd3819957daa81657dcdfed7db79ebbb9db
faiqoh04 <sifaafaiqoh@gmail.com>
2024-09-14 15:01:44 +0700

    JS 3 - Praktikum 5 - Web Lanjut

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ Route::get('/', function() {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-// Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 // Route::get('/user', [UserController::class, 'index']);
