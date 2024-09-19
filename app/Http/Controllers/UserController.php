e5690df7a9fa32786a2a2208da234b780d178655
faiqoh04 <sifaafaiqoh@gmail.com>
2024-09-20 01:22:47 +0700

    JS 4 - Praktikum 2.2 - Web Lanjut

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,9 +48,13 @@ class UserController extends Controller
         // $user = UserModel::find(1);
         // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username', 'nama'], function (){
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function (){
+        //     abort(404);
+        // });
+
+        // PRAKTIKUM 2.2 JS 4
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')-> firstOrFail();
         return view('user', ['data' => $user]);
 
     }
